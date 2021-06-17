23cbdd5a35396b4306be150228e82056ff51f184
Fractal3121 <76220559+Fractal3121@users.noreply.github.com>
2021-06-17 16:23:20 +0300

    University project Intellectual Systems

---
+++ lab 3/lab 3.py
@@ -0,0 +1,311 @@
+from time import time, sleep
+import tracemalloc
+from tkinter import Tk, Canvas
+
+BLOCK_SIZE = 20
+PAC_SIZE = 3
+ANIMATION_SPEED = 0.1
+
+
+def run_alg(func, arg={}):
+    tracemalloc.start()
+    func(**arg) if arg else func()
+    snapshot = tracemalloc.take_snapshot()
+    stats = snapshot.statistics('lineno')
+    total = sum([s.size for s in stats])
+    print(f"\tTotal memory usage: {total / 10 ** 3}MB")
+    tracemalloc.stop()
+
+
+class Game:
+    def __init__(self, field):
+        self.X = len(field[0])
+        self.Y = len(field)
+        self.field = field
+        self.find_elems()
+
+    def visual_path(self, path):
+        tk = Tk()
+        tk.title('Path visualization')
+        tk.resizable(0, 0)  # заборона зміни розміру
+        tk.wm_attributes('-topmost', 1)  # розміщуємо вікно зверху
+        canvas = Canvas(tk, width=self.X * BLOCK_SIZE, height=self.Y * BLOCK_SIZE, highlightthickness=0)
+        canvas.pack()
+        tk.update()
+
+        for point in path:
+            canvas.delete("all")
+            canvas.create_rectangle(  # заливаємо повністю поверхню
+                0, 0, self.X * BLOCK_SIZE, self.Y * BLOCK_SIZE,
+                outline='#fb0', fill='#f50'
+            )
+            for x in range(self.X):
+                for y in range(self.Y):
+                    if self.field[y][x] == '.':  # порожня клітинка
+                        canvas.create_rectangle(
+                            x * BLOCK_SIZE, y * BLOCK_SIZE, (x + 1) * BLOCK_SIZE, (y + 1) * BLOCK_SIZE,
+                            fill='#4d443f', outline='black'
+                        )
+                    elif self.field[y][x] == 'X':  # малюємо стіну
+                        canvas.create_rectangle(
+                            x * BLOCK_SIZE, y * BLOCK_SIZE, (x + 1) * BLOCK_SIZE, (y + 1) * BLOCK_SIZE,
+                            fill='black', outline='blue'
+                        )
+                    else:
+                        pass
+
+            canvas.create_arc(  # малюємо пакмена
+                point[0] * BLOCK_SIZE + PAC_SIZE, point[1] * BLOCK_SIZE + PAC_SIZE,
+                (point[0] + 1) * BLOCK_SIZE - PAC_SIZE, (point[1] + 1) * BLOCK_SIZE - PAC_SIZE,
+                start=30, extent=300, fill='yellow', outline='yellow'
+            )
+
+            canvas.create_oval(  # малюємо ціль
+                path[-1][0] * BLOCK_SIZE + 2 * PAC_SIZE, path[-1][1] * BLOCK_SIZE + 2 * PAC_SIZE,
+                (path[-1][0] + 1) * BLOCK_SIZE - 2 * PAC_SIZE, (path[-1][1] + 1) * BLOCK_SIZE - 2 * PAC_SIZE,
+                fill='orange', outline='green'
+            )
+
+            tk.update()
+            sleep(ANIMATION_SPEED)
+
+        # input()
+        return True
+
+    def test_visual(self):
+        self.visual_path(self.pacman.bfs(self.field, self.target))
+        self.visual_path(self.pacman.dfs(self.field, self.target))
+        self.visual_path(self.pacman.Astar(self.field, self.target))
+        self.visual_path(self.pacman.greedy(self.field, self.target))
+
+    def find_elems(self):
+        for x in range(self.X):
+            for y in range(self.Y):
+                if self.field[y][x] == 'P':
+                    self.pacman = Pacman(x, y)
+                    self.field[y][x] = '.'
+                elif self.field[y][x] == '1':
+                    self.field[y][x] = '.'
+                    self.target = Target(x, y)
+
+    def test_search(self):
+        time_start = time()
+        print(f'Result:', *self.pacman.bfs(self.field, self.target), sep=' > ')
+        print(f'time: {time() - time_start}\n')
+        time_start = time()
+        print(f'Result:', *self.pacman.dfs(self.field, self.target), sep=' > ')
+        print(f'time: {time() - time_start}\n')
+        time_start = time()
+        print(f'Result:', *self.pacman.Astar(self.field, self.target), sep=' > ')
+        print(f'time: {time() - time_start}\n')
+        time_start = time()
+        print(f'Result:', *self.pacman.greedy(self.field, self.target), sep=' > ')
+        print(f'time: {time() - time_start}\n')
+
+    def test_memory(self):
+        run_alg(self.pacman.bfs, {'field': self.field, 'target': self.target})
+        run_alg(self.pacman.dfs, {'field': self.field, 'target': self.target})
+        run_alg(self.pacman.Astar, {'field': self.field, 'target': self.target})
+        run_alg(self.pacman.greedy, {'field': self.field, 'target': self.target})
+
+
+class Target:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        # print(f'Target x={x} y={y}') #консольний вивід для дебагу
+
+
+class Pacman:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        # print(f'Pacman x={x} y={y}') #консольний вивід для дебагу
+
+    # пошук у ширину
+    def bfs(self, field, target):
+        iter_count = 0
+        print('bfs')  # консольний вивід для дебагу
+
+        trgt = (target.x, target.y)  # координати цілі
+        paths = [[(self.x, self.y)]]  # шляхи, по яких проходимо
+        visited = [(self.x, self.y)]  # відвідані вершини
+
+        while paths:
+            buf_paths = []
+
+            # якщо на якомусь шляху потрапляємо в шукану вершину, повертаємо цей шлях
+            for path in paths:
+                if path[-1] == trgt:
+                    print(f'iterations count: {iter_count}')
+                    return path
+
+            # інакше для кожного шляху переглядаємо всі досяжні вершини
+            for path in paths:
+                # досяжні вершини цього шляху
+                next_node = [(path[-1][0] + 1, path[-1][1]), (path[-1][0] - 1, path[-1][1]),
+                             (path[-1][0], path[-1][1] + 1), (path[-1][0], path[-1][1] - 1)]
+
+                # перевірка, чи можемо зайти в цю вершину
+                for node in next_node:
+                    if node in visited:
+                        continue
+                    if field[node[1]][node[0]] == 'X':
+                        continue
+                    # якщо не відвідували та вершина доступна
+                    iter_count += 1
+                    visited += [node]  # позначаємо вершину відвіданою
+                    buf_paths.append(path + [node])  # додаємо шлях із новою вершиною
+
+            paths = buf_paths
+            # консольний вивід для дебагу
+            # print('**********************************************************************')
+            # print(*paths, sep='\n')
+
+        return False  # якщо дійшли сюди, то побудувати шлях не вийшло
+
+    # пошук у глибину
+    def dfs(self, field, target):
+        iter_count = 0
+        print('dfs')  # консольний вивід для дебагу
+
+        trgt = (target.x, target.y)  # координати цілі
+        path = [(self.x, self.y)]  # шлях, по якому проходимо
+        visited = [(self.x, self.y)]  # відвідані вершини
+
+        while path:
+            # повертаємо шлях, якщо потрапили в шукану вершину
+            if path[-1] == trgt:
+                print(f'iterations count: {iter_count}')
+                return path
+
+            # шукаємо наступну досяжну вершину
+            next_nodes = [(path[-1][0] + 1, path[-1][1]), (path[-1][0] - 1, path[-1][1]),
+                          (path[-1][0], path[-1][1] + 1), (path[-1][0], path[-1][1] - 1)]
+            for node in next_nodes:
+                # якщо не були в цій вершині та вона доступна
+                if node not in visited and field[node[1]][node[0]] != 'X':
+                    visited += [node]  # відмічаємо вершину відвіданою
+                    path.append(node)  # переходимо в неї
+                    iter_count += 1
+                    break  # завершуємо ітерацію
+            else:
+                # якщо з теперішньої вершини не можемо перейти в наступну, то повертаємося назад
+                path.pop()
+
+            # консольний вивід для дебагу
+            # print(*path, sep=' > ')
+
+        return False  # якщо дійшли сюди, то побудувати шлях не вийшло
+
+    # евристика для А*
+    def h(self, start, end):
+        return ((start[0] - end[0]) ** 2 + (start[1] - end[1]) ** 2) ** 0.5
+
+    # A*
+    def Astar(self, field, target):
+
+        iter_count = 0
+        print('A*')  # консольний вивід для дебагу
+
+        trgt = (target.x, target.y)  # координати цілі
+        start = (self.x, self.y)  # початкова вершина
+        closed = []  # досліджені вершини
+        open = [start]  # відомі вершини
+        route = {}  # з якої вершини перейшли
+        g = {start: 0}  # ціна переходу
+        f = {start: g[start] + self.h(start, trgt)}  # ф-ція ціни маршруту
+
+        while open:
+            cur = min(open, key=lambda x: f[x])  # обираємо вершину з найменшим f
+            # print(cur, f[cur]) #консольний вивід для дебагу
+
+            if cur == trgt:  # якщо потрапили в шукану вершину, відновлюємо шлях
+                res = [trgt]
+                while res[-1] != start:
+                    res.append(route[res[-1]])
+                print(f'iterations count: {iter_count}')
+                return res[::-1]
+
+            open.remove(cur)
+            closed.append(cur)
+
+            # досліджуємо всіх сусідів
+            neighbours = [(cur[0] + 1, cur[1]), (cur[0] - 1, cur[1]), (cur[0], cur[1] + 1), (cur[0], cur[1] - 1)]
+            for neighbour in neighbours:
+                if field[neighbour[1]][neighbour[0]] == 'X':  # не можемо перейти у вершину
+                    continue
+                if neighbour in closed:  # вершина вже досліджена
+                    continue
+
+                # вартість переходу до нащадка
+                temp_g = g[cur] + 1  # dist(cur, neigbour) == const == 1
+                # якщо нащадок не відомий або шлях до нього через теперішню вершину дешевший
+                # змінюємо маршрут
+                if (neighbour not in open) or (temp_g < g[neighbour]):
+                    route[neighbour] = cur
+                    g[neighbour] = temp_g
+                    f[neighbour] = g[neighbour] + self.h(neighbour, trgt)
+                    iter_count += 1
+                if neighbour not in open:  # додаємо вершину до відомих
+                    open.append(neighbour)
+
+        return False  # якщо дійшли сюди, то побудувати шлях не вийшло
+
+    # жадібний алгоритм Дейкстрим
+    def greedy(self, field, target):
+        iter_count = 0
+        print('greedy')  # консольний вивід для дебагу
+
+        trgt = (target.x, target.y)  # координати цілі
+        start = (self.x, self.y)  # початкова вершина
+        enable = {start}  # вершини з вартістю переходу в них <inf
+        visited = set()
+        route = {}  # з якої вершини перейшли
+        d = {start: 0}  # ціна шляху у вершину
+
+        while enable:
+            cur = min(enable - visited, key=lambda x: d[x])  # обираємо вершину, у якій ще не були, з найменшим d
+            visited.add(cur)
+
+            if cur == trgt:  # якщо потрапили в шукану вершину, відновлюємо шлях
+                res = [trgt]
+                while res[-1] != start:
+                    res.append(route[res[-1]])
+                print(f'iterations count: {iter_count}')
+                return res[::-1]
+
+            # досліджуємо всіх сусідів
+            neighbours = [(cur[0] + 1, cur[1]), (cur[0] - 1, cur[1]), (cur[0], cur[1] + 1), (cur[0], cur[1] - 1)]
+            for neighbour in neighbours:
+                # не можемо перейти у вершину
+                if field[neighbour[1]][neighbour[0]] == 'X':
+                    continue
+
+                # вартість переходу до нащадка
+                temp_d = d[cur] + 1  # dist(cur, neigbour) == const == 1
+                # якщо шлях до нащадка через теперішню вершину дешевший
+                # змінюємо маршрут
+                # neighbour not in enable <=> d[neighbour]==inf
+                if (neighbour not in enable) or (temp_d < d[neighbour]):
+                    route[neighbour] = cur
+                    d[neighbour] = temp_d
+                    enable.add(neighbour)
+                    iter_count += 1
+
+        return False  # якщо дійшли сюди, то побудувати шлях не вийшло
+
+
+def read_field(inp):
+    with open(inp) as sr:
+        x = sr.read().splitlines()
+    field = [[s for s in line] for line in x]
+    return Game(field)
+
+
+if __name__ == '__main__':
+    game = read_field('big_field.txt')
+    # print(*game.field, game.X, game.Y, sep='\n')
+    game.test_memory()
+    game.test_search()
+    game.test_visual()
---
+++ lab 6/lab 6.py
@@ -0,0 +1,273 @@
+from time import sleep
+import random
+from tkinter import Tk, Canvas
+
+SCORE_FOR_POINT = 5  # Винагорода за з'їдену ціль
+RANDOM_MOVE = 5  # К-сть ходів привидів перед рандомним ходом одного з них
+BADLENESS = 9  # Штраф за смерть пакмена
+SPIRIT_PROFIT = 2  # Винагорода за з'їденого пакмена
+INF = 99999999999999999999999999999999999999999999999
+BLOCK_SIZE = 20  # Розмір блоку для візуалізації
+PAC_SIZE = 3  # На скільки пікселів пакмен менший блоків
+ANIMATION_SPEED = 0.5  # Час затримки між кроками
+DEEP = 10  # Глибина пошуку стратегій
+POINTS_COUNT = 0  # К-сть точок на рівні, які треба з'їсти
+eated_points = 0  # К-сть з'їдених точок
+random_move_counter = 0  # К-сть зроблених кроків привидами
+field = []  # Поле
+X, Y = 21, 27  # Розміри поля
+levels = [
+    'level_1_0.txt',
+   #  'level_1_1.txt',    #    На цьому рівні пакмен точно програє
+   # 'level_1_2.txt',
+   # 'level_1_3.txt',
+    'level_2_0.txt',
+   # 'level_2_1.txt',
+   # 'level_2_2.txt',
+]
+pacman = (0, 0)  # Координати пакмена
+spirits = []  # Координати привидів
+
+tk = Tk()  # Штука для графіки
+tk.title('Pacman')
+tk.resizable(0, 0)  # заборона зміни розміру
+tk.wm_attributes('-topmost', 1)  # розміщуємо вікно зверху
+canvas = Canvas(tk, width=X * BLOCK_SIZE, height=Y * BLOCK_SIZE, highlightthickness=0)
+canvas.pack()
+tk.update()
+
+
+def visual_move(pcmn, sprts):  # Візуалізація поля з пакменом та привидами
+    canvas.delete("all")
+    canvas.create_rectangle(  # Заливаємо повністю поверхню
+        0, 0, X * BLOCK_SIZE, Y * BLOCK_SIZE,
+        outline='#fb0', fill='#f50'
+    )
+    for x in range(X):
+        for y in range(Y):
+            if field[y][x] == '.':  # Порожня клітинка
+                canvas.create_rectangle(
+                    x * BLOCK_SIZE, y * BLOCK_SIZE, (x + 1) * BLOCK_SIZE, (y + 1) * BLOCK_SIZE,
+                    fill='#4d443f', outline='black'
+                )
+            elif field[y][x] == 'X':  # Малюємо стіну
+                canvas.create_rectangle(
+                    x * BLOCK_SIZE, y * BLOCK_SIZE, (x + 1) * BLOCK_SIZE, (y + 1) * BLOCK_SIZE,
+                    fill='black', outline='blue'
+                )
+            elif field[y][x] == '1':
+                canvas.create_oval(  # Малюємо ціль
+                    x * BLOCK_SIZE + 2 * PAC_SIZE, y * BLOCK_SIZE + 2 * PAC_SIZE,
+                    (x + 1) * BLOCK_SIZE - 2 * PAC_SIZE, (y + 1) * BLOCK_SIZE - 2 * PAC_SIZE,
+                    fill='orange', outline='green'
+                )
+            else:
+                pass
+
+    canvas.create_arc(  # Малюємо пакмена
+        pcmn[0] * BLOCK_SIZE + PAC_SIZE, pcmn[1] * BLOCK_SIZE + PAC_SIZE,
+        (pcmn[0] + 1) * BLOCK_SIZE - PAC_SIZE, (pcmn[1] + 1) * BLOCK_SIZE - PAC_SIZE,
+        start=30, extent=300, fill='yellow', outline='yellow'
+    )
+
+    colors = ['blue', 'pink', 'gray', ]
+    for i in range(len(sprts)):
+        canvas.create_arc(  # Малюємо привида
+            sprts[i][0] * BLOCK_SIZE + PAC_SIZE, sprts[i][1] * BLOCK_SIZE + PAC_SIZE,
+            (sprts[i][0] + 1) * BLOCK_SIZE - PAC_SIZE, (sprts[i][1] + 1) * BLOCK_SIZE - PAC_SIZE,
+            start=300, extent=300, fill=colors[i], outline=colors[i]
+        )
+
+    tk.update()
+    sleep(ANIMATION_SPEED)
+
+
+def read_field(inp):  # Зчитує поле з файлу
+    global field
+    global X
+    global Y
+
+    with open(inp) as sr:
+        x = sr.read().splitlines()
+    field = [[s for s in line] for line in x]
+
+    X = len(field[0])
+    Y = len(field)
+
+
+def find_elems():  # Шукає координати пакмена та привидів
+    global pacman
+    global spirits
+    global POINTS_COUNT
+
+    POINTS_COUNT = 0
+    spirits = []
+
+    for x in range(X):
+        for y in range(Y):
+            if field[y][x] == 'P':
+                pacman = (x, y)
+                field[y][x] = '.'
+            elif field[y][x] == '1':
+                POINTS_COUNT += 1
+            elif field[y][x] == 'S':
+                field[y][x] = '.'
+                spirits.append((x, y))
+
+
+def possible_moves(point):  # Шукає всі досяжні вершини із заданої точки
+    next_nodes = [(point[0] + 1, point[1]), (point[0] - 1, point[1]),
+                  (point[0], point[1] + 1), (point[0], point[1] - 1)]
+    return [node for node in next_nodes if field[node[1]][node[0]] != 'X']
+
+
+def minimax(position, depth=0, maximizing=True, score=0, prev_pos=()):
+    #   Мінімаксний алгоритм пошуку оптимального шляху
+
+    stop_game = False
+    if prev_pos and depth % 2 == 0:  # Перевірка, чи з'їдають привиди пакмена
+        for i in range(len(position[1])):
+            if position[1][i] == position[0] or \
+                    (position[1][i] == prev_pos[0] and prev_pos[1][i] == position[0]):
+                stop_game = True
+                score -= BADLENESS ** (DEEP - depth)
+
+    if depth % 2 == 0:  # І пакмен, і привиди зробили крок
+        prev_pos = position
+
+    #   Пакмен з'їдає точку
+    if field[position[0][1]][position[0][0]] == '1':
+        score += SCORE_FOR_POINT
+        if eated_points + score // SCORE_FOR_POINT >= POINTS_COUNT:
+            #   Пакмен з'їв усі точки
+            score += SCORE_FOR_POINT ** (DEEP - depth)
+            stop_game = True
+
+    if depth == DEEP or stop_game:
+        return (score, position[0], position[1])
+
+    if maximizing:  # This is pacman's move
+        #   Можливі ходи пакмена та рахунок, який він може отримати
+        pac_benefit = {move: minimax((move, position[1]), depth + 1, False, score, prev_pos)[0] \
+                       for move in possible_moves(position[0])}
+        max_benefit = max(pac_benefit.values())
+        best_strategies = list(filter(lambda x: pac_benefit[x] == max_benefit, pac_benefit.keys()))
+        pacman_next = random.choice(best_strategies)
+        return (max_benefit, pacman_next, position[1])
+    else:  # This is spirits move
+        #   Заповнюємо всі можливі комбінації переміщень привидів
+        sp_pos_moves = [[]]
+        for spirit in position[1]:
+            buf = []
+            for move in possible_moves(spirit):
+                buf += [m + [move] for m in sp_pos_moves]
+            sp_pos_moves = buf
+        #   Переміщення привидів та рахунок, який можна здобути
+        sp_benefit = {tuple(move): minimax((position[0], move), depth + 1, True, score, prev_pos)[0] \
+                      for move in sp_pos_moves}
+        min_benefit = min(sp_benefit.values())
+        best_strategies = list(filter(lambda x: sp_benefit[x] == min_benefit, sp_benefit.keys()))
+        sp_next = random.choice(best_strategies)
+        return (min_benefit, position[0], sp_next)
+    return (score, pacman, spirits)
+
+
+def alpha_beta(position, depth=0, maximizing=True, score=0, prev_pos=(), alpha=-INF, beta=INF):
+    #   Альфа-бета відтинання
+
+    stop_game = False
+    if prev_pos and depth % 2 == 0:  # Перевірка, чи з'їдають привиди пакмена
+        for i in range(len(position[1])):
+            if position[1][i] == position[0] or \
+                    (position[1][i] == prev_pos[0] and prev_pos[1][i] == position[0]):
+                stop_game = True
+                score -= BADLENESS ** (DEEP - depth)
+
+    if depth % 2 == 0:  # І пакмен, і привиди зробили крок
+        prev_pos = position
+
+    #   Пакмен з'їдає точку
+    if field[position[0][1]][position[0][0]] == '1':
+        score += SCORE_FOR_POINT
+        if eated_points + score // SCORE_FOR_POINT >= POINTS_COUNT:
+            #   Пакмен з'їв усі точки
+            score += SCORE_FOR_POINT ** (DEEP - depth)
+            stop_game = True
+
+    if depth == DEEP or stop_game:
+        return (score, position[0], position[1])
+
+    if maximizing:  # This is pacman's move
+        max_eval = -INF
+        pac_benefit = {}  # Можливі комбінації
+        for move in possible_moves(position[0]):
+            ev = alpha_beta((move, position[1]), depth + 1, False, score, prev_pos, alpha, beta)[0]
+            pac_benefit[move] = ev
+            max_eval = max(max_eval, ev)
+            alpha = max(alpha, ev)
+            if beta <= alpha:
+                break
+
+        max_benefit = max(pac_benefit.values())
+        best_strategies = list(filter(lambda x: pac_benefit[x] == max_benefit, pac_benefit.keys()))
+        pacman_next = best_strategies[0] if len(best_strategies) != len(pac_benefit) else random.choice(best_strategies)
+        return (max_eval, pacman_next, position[1])
+
+    else:  # This is spirits move
+        sp_pos_moves = [[]]
+        for spirit in position[1]:
+            buf = []
+            for move in possible_moves(spirit):
+                buf += [m + [move] for m in sp_pos_moves]
+            sp_pos_moves = buf
+
+        min_eval = INF
+        sp_benefit = {}  # Можливі комбінації
+        for move in sp_pos_moves:
+            ev = alpha_beta((position[0], move), depth + 1, True, score, prev_pos, alpha, beta)[0]
+            sp_benefit[tuple(move)] = ev
+            min_eval = min(min_eval, ev)
+            beta = min(beta, ev)
+            if beta <= alpha:
+                break
+
+        min_benefit = min(sp_benefit.values())
+        best_strategies = list(filter(lambda x: sp_benefit[x] == min_benefit, sp_benefit.keys()))
+        sp_next = best_strategies[0] if len(best_strategies) != len(sp_benefit) else random.choice(best_strategies)
+        return (min_eval, position[0], sp_next)
+    return (score, pacman, spirits)
+
+
+#######################################################################################
+PAC_ALIVE = True
+for level in levels:
+    read_field(level)
+    find_elems()
+    eated_points = 0
+
+    if not PAC_ALIVE:
+        sleep(2)
+        break
+
+    visual_move(pacman, spirits)
+    while PAC_ALIVE:
+        f = alpha_beta
+        pacman_next = f((pacman, spirits))[1]
+        spirits_next = f((pacman, spirits), maximizing=False)[2]
+
+        if field[pacman_next[1]][pacman_next[0]] == '1':
+            eated_points += 1
+            field[pacman_next[1]][pacman_next[0]] = '.'
+            if eated_points >= POINTS_COUNT:
+                visual_move(pacman_next, spirits)
+                sleep(2)
+                break
+
+        for i in range(len(spirits_next)):
+            if pacman_next == spirits_next[i] or \
+                    (pacman_next == spirits[i] and pacman == spirits_next[i]):
+                PAC_ALIVE = False
+
+        pacman = pacman_next
+        spirits = spirits_next
+        visual_move(pacman_next, spirits_next)
